2db195afe74d34b3bffb0f6119579212b0d7b758
Frank Bartunek <frank.bartunek@gmail.com>
2025-01-01 14:09:02 -0600

    Extensively commented to explain to people who read my code what on earth I'm doing

--- monkeytype.c
+++ monkeytype.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <limits.h>
 #define ALPHASTART 65
-#define ALPHALENGTH 56
-
+#define ALPHALENGTH 54
+#define SPACEPROBABLE 8
 int main(int argc, char* argv)
 {
 	srand(time(0));
@@ -15,11 +15,30 @@ int main(int argc, char* argv)
 	let=malloc(sizeof(int));			//Make sure we actually have memory
 	while(ii<INT_MAX && stop==0)
 	{
-		*(int*)let=ALPHASTART+( rand() % ALPHALENGTH);
-		printf("%c",*(char*)let);
-		ii++;
-	}
-	free(let);
-	return 0;
+		*(int*)let=ALPHASTART+( rand() % ALPHALENGTH)-SPACEPROBABLE;
+	/* Generate a random number, and put it into our letter slot */
+		if(*(int*)let<ALPHASTART) 
+/* Spaces normally throw a bit of a monkey wrench into our whole typing 
+thing, as space is assigned a value much lower than actual letters of 
+the english alphabet.  Space is assigned a value of 20 in ASCII, whereas 
+the letter A starts at 65.  By assigning numbers lower than 65 as space, 
+I split the difference and allow a degree of control as to what is and 
+is not a space with the 'Spaceprobable' variable.  I am also avoiding printing
+out control characters this way.  */
 
+		{
+		printf(" ");
+		}
+		else
+		{
+/* If we have a number in our range of actual letters, we once again 
+tell the computer to trust me, take this variable, and print it out as a 
+'letter'.  Even though type-wise, I've given it no previous evidence of 
+this. */
+		printf("%c",*(char*)let);
+		}
+		ii++;    // Loop to next
+	}
+	free(let);   //Stop reserving the letter when we're done
+	return 0;   // Operating system assumes 0 means "no errors."
 }
