56245cbaf642db84e98e42ce3cced3d661b0a8e1
Frank Bartunek <frank.bartunek@gmail.com>
2025-01-01 13:50:32 -0600

    First release

---
+++ monkeytype.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#define ALPHASTART 65
+#define ALPHALENGTH 56
+
+int main(int argc, char* argv)
+{
+	srand(time(0));
+	srand(rand());
+	void *let;					 // Random 'Letter'
+	int ii=0;					 //Counter varible
+	int stop=0;					 // Stop signal
+	while(ii<INT_MAX && stop==0)
+	{
+		*(int*)let=ALPHASTART+( rand() % ALPHALENGTH);
+		printf("%c",*(char*)let);
+		ii++;
+	}
+	return 0;
+}
